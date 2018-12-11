2ae035a94f31ceab277761919deb001cf101feb8
kusumakarb <kusumakarbvss@gmail.com>
2018-12-11 12:30:54 +0530

    dataworld disabled

--- src/main/scala/DataWorld.scala
+++ src/main/scala/DataWorld.scala
@@ -11,8 +11,7 @@ object DataWorld extends App {
     .getOrCreate()
 
   val username = "kusumakarb"
-  val token =
-    "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwcm9kLXVzZXItY2xpZW50Omt1c3VtYWthcmIiLCJpc3MiOiJhZ2VudDprdXN1bWFrYXJiOjo1NDNlYTc3My0xNTI3LTQ1OTgtYjJiYi0wZTQ4YTRmOTlhMmMiLCJpYXQiOjE1NDI5MTAyMTQsInJvbGUiOlsidXNlcl9hcGlfcmVhZCIsInVzZXJfYXBpX3dyaXRlIl0sImdlbmVyYWwtcHVycG9zZSI6dHJ1ZX0._NJHnzmIQgMg-vtwsidCf1tU7HhphJs9jcnns9ZHmmMmBaTJAFmQAO7DaG2RcXzhEy4qrYHt6NoumjGqLVFGlQ"
+  val token = "Replace this with the actual token!!!"
 
   val df = spark.read
     .format("jdbc")
